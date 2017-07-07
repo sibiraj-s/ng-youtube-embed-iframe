fea7a0b85b84a9ca491c78ecea3060a01a9b14c7
Sibiraj <sibiraj_s@live.com>
2017-07-07 17:48:44 +0530

    perf: modular approach

--- dist/ng-youtube.js
+++ dist/ng-youtube.js
@@ -1,126 +1,141 @@
+
+/*
+ * @module ng-youtube-embed-iframe
+ * @description The AngularJS directive for youtube-iframe-player-api
+ * @version v1.1.2
+ * @link https://github.com/Sibiraj-S/ng-youtube-embed-iframe
+ * @licence MIT License, https://opensource.org/licenses/MIT
+ */
+
 (function() {
   'use strict';
+  var $youtube, $youtubePlayerConfig;
+
+  $youtubePlayerConfig = function() {
+    return {
+      width: '100%',
+      height: '100%',
+      playerVars: {}
+    };
+  };
+
+  $youtube = function($compile, ytPlayer, youtubePlayerConfig, $window) {
+    return {
+      restrict: 'EA',
+      transclude: true,
+      template: '<div id="ngYoutube"></div>',
+      scope: {
+        height: '@',
+        width: '@',
+        videoId: '@',
+        playerOptions: '=?'
+      },
+      link: function(scope) {
+
+        /*
+         * load the Youtube IFrame Player API code asynchronously
+         * load page only once
+         */
+        var firstScriptTag, onApiChange, onPlaybackRateChange, onPlayerError, onPlayerPlaybackQualityChange, onPlayerReady, onPlayerStateChange, playerHeight, playerVars, playerVideoId, playerWidth, tag, tempHeight, tempWidth;
+        if (typeof $window.YT === 'undefined' || typeof $window.YT.Player === 'undefined') {
+          tag = document.createElement('script');
+          tag.src = 'https://www.youtube.com/iframe_api';
+          firstScriptTag = document.getElementsByTagName('script')[0];
+          firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        }
+
+        /*
+         * check for parameters assigned to the element
+         * if assigned use the parameters
+         * else if use parameters are defined in options
+         * else use default parameters
+         */
+        scope.playerOptions = scope.playerOptions ? scope.playerOptions : {};
+        playerVideoId = scope.videoId ? scope.videoId : scope.playerOptions.videoId;
+        playerHeight = scope.height ? scope.height : (tempHeight = scope.playerOptions.height ? scope.playerOptions.height : youtubePlayerConfig.height);
+        playerWidth = scope.width ? scope.width : (tempWidth = scope.playerOptions.width ? scope.playerOptions.width : youtubePlayerConfig.width);
+        playerVars = scope.playerOptions.playerVars ? scope.playerOptions.playerVars : youtubePlayerConfig.playerVars;
+
+        /*
+         * create iframe and append video url to it
+         */
+        $window.onYouTubeIframeAPIReady = function() {
+          ytPlayer = new $window.YT.Player('ngYoutube', {
+            videoId: playerVideoId,
+            height: playerHeight,
+            width: playerWidth,
+            playerVars: playerVars,
+            events: {
+              onReady: onPlayerReady,
+              onPlaybackQualityChange: onPlayerPlaybackQualityChange,
+              onStateChange: onPlayerStateChange,
+              onError: onPlayerError,
+              onPlaybackRateChange: onPlaybackRateChange,
+              onApiChange: onApiChange
+            }
+          });
+        };
+
+        /*
+         * events emit by youtube iframe api
+         */
+        onPlayerReady = function(event) {
+          scope.$emit('ngYoutubePlayer:onPlayerReady', event);
+        };
+        onPlayerPlaybackQualityChange = function(event) {
+          scope.$emit('ngYoutubePlayer:onPlayerPlaybackQualityChange', event);
+        };
+        onPlayerStateChange = function(event) {
+          scope.$emit('ngYoutubePlayer:onPlayerStateChange', event);
+          switch (event.data) {
+            case $window.YT.PlayerState.PLAYING:
+              scope.$emit('ngYoutubePlayer:PLAYING', event);
+              break;
+            case $window.YT.PlayerState.ENDED:
+              scope.$emit('ngYoutubePlayer:ENDED', event);
+              break;
+            case $window.YT.PlayerState.UNSTARTED:
+              scope.$emit('ngYoutubePlayer:UNSTARTED', event);
+              break;
+            case $window.YT.PlayerState.PAUSED:
+              scope.$emit('ngYoutubePlayer:PAUSED', event);
+              break;
+            case $window.YT.PlayerState.BUFFERING:
+              scope.$emit('ngYoutubePlayer:BUFFERING', event);
+              break;
+            case $window.YT.PlayerState.CUED:
+              scope.$emit('ngYoutubePlayer:CUED', event);
+          }
+        };
+        onPlayerError = function(event) {
+          scope.$emit('ngYoutubePlayer:onPlayerError', event);
+        };
+        onPlaybackRateChange = function(event) {
+          scope.$emit('ngYoutubePlayer:onPlaybackRateChange', event);
+        };
+        onApiChange = function() {
+          scope.$emit('ngYoutubePlayer:onApiChange', ytPlayer);
+        };
+        scope.$on('$destroy', function() {
+          $window.ytPlayer && $window.ytPlayer.destroy();
+          $window.YT = void 0;
+        });
+      }
+    };
+  };
+
+
+  /*
+   * dependency injection
+   */
+
+  $youtube.$inject = ['$compile', 'ytPlayer', 'youtubePlayerConfig', '$window'];
+
 
   /*
    * define angular application
    */
-  var app;
 
-  app = angular.module('ngYoutube', ['ngSanitize']);
-
-  app.constant('ytPlayer');
-
-  app.constant('youtubePlayerConfig', {
-    width: '100%',
-    height: '100%',
-    playerVars: {}
-  });
-
-  app.directive('youtube', [
-    '$compile', 'ytPlayer', 'youtubePlayerConfig', '$window', function($compile, ytPlayer, youtubePlayerConfig, $window) {
-      return {
-        restrict: 'EA',
-        transclude: true,
-        template: '<div id="ngYoutube"></div>',
-        scope: {
-          height: '@',
-          width: '@',
-          videoId: '@',
-          playerOptions: '=?'
-        },
-        link: function(scope) {
-
-          /*
-           * load the Youtube IFrame Player API code asynchronously
-           * load page only once
-           */
-          var firstScriptTag, onApiChange, onPlaybackRateChange, onPlayerError, onPlayerPlaybackQualityChange, onPlayerReady, onPlayerStateChange, playerHeight, playerVars, playerVideoId, playerWidth, tag, tempHeight, tempWidth;
-          if (typeof $window.YT === 'undefined' || typeof $window.YT.Player === 'undefined') {
-            tag = document.createElement('script');
-            tag.src = 'https://www.youtube.com/iframe_api';
-            firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-          }
-
-          /*
-           * check for parameters assigned to the element
-           * if assigned use the parameters
-           * else if use parameters are defined in options
-           * else use default parameters
-           */
-          scope.playerOptions = scope.playerOptions ? scope.playerOptions : {};
-          playerVideoId = scope.videoId ? scope.videoId : scope.playerOptions.videoId;
-          playerHeight = scope.height ? scope.height : (tempHeight = scope.playerOptions.height ? scope.playerOptions.height : youtubePlayerConfig.height);
-          playerWidth = scope.width ? scope.width : (tempWidth = scope.playerOptions.width ? scope.playerOptions.width : youtubePlayerConfig.width);
-          playerVars = scope.playerOptions.playerVars ? scope.playerOptions.playerVars : youtubePlayerConfig.playerVars;
-
-          /*
-           * create iframe and append video url to it
-           */
-          $window.onYouTubeIframeAPIReady = function() {
-            ytPlayer = new $window.YT.Player('ngYoutube', {
-              videoId: playerVideoId,
-              height: playerHeight,
-              width: playerWidth,
-              playerVars: playerVars,
-              events: {
-                onReady: onPlayerReady,
-                onPlaybackQualityChange: onPlayerPlaybackQualityChange,
-                onStateChange: onPlayerStateChange,
-                onError: onPlayerError,
-                onPlaybackRateChange: onPlaybackRateChange,
-                onApiChange: onApiChange
-              }
-            });
-          };
-
-          /*
-           * events emit by youtube iframe api
-           */
-          onPlayerReady = function(event) {
-            scope.$emit('ngYoutubePlayer:onPlayerReady', event);
-          };
-          onPlayerPlaybackQualityChange = function(event) {
-            scope.$emit('ngYoutubePlayer:onPlayerPlaybackQualityChange', event);
-          };
-          onPlayerStateChange = function(event) {
-            scope.$emit('ngYoutubePlayer:onPlayerStateChange', event);
-            switch (event.data) {
-              case $window.YT.PlayerState.PLAYING:
-                scope.$emit('ngYoutubePlayer:PLAYING', event);
-                break;
-              case $window.YT.PlayerState.ENDED:
-                scope.$emit('ngYoutubePlayer:ENDED', event);
-                break;
-              case $window.YT.PlayerState.UNSTARTED:
-                scope.$emit('ngYoutubePlayer:UNSTARTED', event);
-                break;
-              case $window.YT.PlayerState.PAUSED:
-                scope.$emit('ngYoutubePlayer:PAUSED', event);
-                break;
-              case $window.YT.PlayerState.BUFFERING:
-                scope.$emit('ngYoutubePlayer:BUFFERING', event);
-                break;
-              case $window.YT.PlayerState.CUED:
-                scope.$emit('ngYoutubePlayer:CUED', event);
-            }
-          };
-          onPlayerError = function(event) {
-            scope.$emit('ngYoutubePlayer:onPlayerError', event);
-          };
-          onPlaybackRateChange = function(event) {
-            scope.$emit('ngYoutubePlayer:onPlaybackRateChange', event);
-          };
-          onApiChange = function() {
-            scope.$emit('ngYoutubePlayer:onApiChange', ytPlayer);
-          };
-          scope.$on('$destroy', function() {
-            $window.ytPlayer && $window.ytPlayer.destroy();
-            $window.YT = void 0;
-          });
-        }
-      };
-    }
-  ]);
+  angular.module('ngYoutube', ['ngSanitize']).directive('youtube', $youtube).constant('youtubePlayerConfig', $youtubePlayerConfig).constant('ytPlayer');
 
 }).call(this);
--- src/ng-youtube.coffee
+++ src/ng-youtube.coffee
@@ -1,129 +1,139 @@
+###
+# @module ng-youtube-embed-iframe
+# @description The AngularJS directive for youtube-iframe-player-api
+# @version v1.1.2
+# @link https://github.com/Sibiraj-S/ng-youtube-embed-iframe
+# @licence MIT License, https://opensource.org/licenses/MIT
+###
+
 'use strict'
 
-###
-# define angular application
-###
-
-app = angular.module 'ngYoutube', ['ngSanitize']
-
-app.constant 'ytPlayer'
-
-app.constant 'youtubePlayerConfig',
+$youtubePlayerConfig = ->
   width: '100%'
   height: '100%'
   playerVars: {}
 
-app.directive 'youtube',
-['$compile', 'ytPlayer', 'youtubePlayerConfig', '$window',
-  ($compile, ytPlayer, youtubePlayerConfig, $window) ->
-    restrict: 'EA'
-    transclude: true
-    template: '<div id="ngYoutube"></div>'
-    scope:
-      height: '@'
-      width: '@'
-      videoId: '@'
-      playerOptions: '=?'
-    link: (scope) ->
+$youtube = ($compile, ytPlayer, youtubePlayerConfig, $window) ->
+  restrict: 'EA'
+  transclude: true
+  template: '<div id="ngYoutube"></div>'
+  scope:
+    height: '@'
+    width: '@'
+    videoId: '@'
+    playerOptions: '=?'
+  link: (scope) ->
 
-      ###
-      # load the Youtube IFrame Player API code asynchronously
-      # load page only once
-      ###
+    ###
+    # load the Youtube IFrame Player API code asynchronously
+    # load page only once
+    ###
 
-      if typeof $window.YT is 'undefined' or typeof $window.YT.Player is 'undefined'
-        tag = document.createElement('script')
-        tag.src = 'https://www.youtube.com/iframe_api'
-        firstScriptTag = document.getElementsByTagName('script')[0]
-        firstScriptTag.parentNode.insertBefore tag, firstScriptTag
+    if typeof $window.YT is 'undefined' or typeof $window.YT.Player is 'undefined'
+      tag = document.createElement('script')
+      tag.src = 'https://www.youtube.com/iframe_api'
+      firstScriptTag = document.getElementsByTagName('script')[0]
+      firstScriptTag.parentNode.insertBefore tag, firstScriptTag
 
-      ###
-      # check for parameters assigned to the element
-      # if assigned use the parameters
-      # else if use parameters are defined in options
-      # else use default parameters
-      ###
+    ###
+    # check for parameters assigned to the element
+    # if assigned use the parameters
+    # else if use parameters are defined in options
+    # else use default parameters
+    ###
 
-      scope.playerOptions = if scope.playerOptions then scope.playerOptions else {}
+    scope.playerOptions = if scope.playerOptions then scope.playerOptions else {}
 
-      playerVideoId = if scope.videoId then scope.videoId else
-        scope.playerOptions.videoId
+    playerVideoId = if scope.videoId then scope.videoId else
+      scope.playerOptions.videoId
 
-      playerHeight = if scope.height then scope.height else
-        (tempHeight = if scope.playerOptions.height then scope.playerOptions.height else
-          youtubePlayerConfig.height)
+    playerHeight = if scope.height then scope.height else
+      (tempHeight = if scope.playerOptions.height then scope.playerOptions.height else
+        youtubePlayerConfig.height)
 
-      playerWidth = if scope.width then scope.width else
-        (tempWidth = if scope.playerOptions.width then scope.playerOptions.width else
-          youtubePlayerConfig.width)
+    playerWidth = if scope.width then scope.width else
+      (tempWidth = if scope.playerOptions.width then scope.playerOptions.width else
+        youtubePlayerConfig.width)
 
-      playerVars = if scope.playerOptions.playerVars then scope.playerOptions.playerVars else
-        youtubePlayerConfig.playerVars
+    playerVars = if scope.playerOptions.playerVars then scope.playerOptions.playerVars else
+      youtubePlayerConfig.playerVars
 
-      ###
-      # create iframe and append video url to it
-      ###
+    ###
+    # create iframe and append video url to it
+    ###
 
-      $window.onYouTubeIframeAPIReady = ->
-        ytPlayer = new $window.YT.Player 'ngYoutube',
-          videoId: playerVideoId
-          height: playerHeight
-          width: playerWidth
-          playerVars: playerVars
-          events:
-            onReady: onPlayerReady
-            onPlaybackQualityChange: onPlayerPlaybackQualityChange
-            onStateChange: onPlayerStateChange
-            onError: onPlayerError
-            onPlaybackRateChange: onPlaybackRateChange
-            onApiChange: onApiChange
-        return
-      ###
-      # events emit by youtube iframe api
-      ###
-
-      onPlayerReady = (event) ->
-        scope.$emit 'ngYoutubePlayer:onPlayerReady', event
-        return
-
-      onPlayerPlaybackQualityChange = (event) ->
-        scope.$emit 'ngYoutubePlayer:onPlayerPlaybackQualityChange', event
-        return
-
-      onPlayerStateChange = (event) ->
-        scope.$emit 'ngYoutubePlayer:onPlayerStateChange', event
-
-        switch event.data
-          when $window.YT.PlayerState.PLAYING
-            scope.$emit 'ngYoutubePlayer:PLAYING', event
-          when $window.YT.PlayerState.ENDED
-            scope.$emit 'ngYoutubePlayer:ENDED', event
-          when $window.YT.PlayerState.UNSTARTED
-            scope.$emit 'ngYoutubePlayer:UNSTARTED', event
-          when $window.YT.PlayerState.PAUSED
-            scope.$emit 'ngYoutubePlayer:PAUSED', event
-          when $window.YT.PlayerState.BUFFERING
-            scope.$emit 'ngYoutubePlayer:BUFFERING', event
-          when $window.YT.PlayerState.CUED
-            scope.$emit 'ngYoutubePlayer:CUED', event
-        return
-
-      onPlayerError = (event) ->
-        scope.$emit 'ngYoutubePlayer:onPlayerError', event
-        return
-
-      onPlaybackRateChange = (event) ->
-        scope.$emit 'ngYoutubePlayer:onPlaybackRateChange', event
-        return
-
-      onApiChange = ->
-        scope.$emit 'ngYoutubePlayer:onApiChange', ytPlayer
-        return
-
-      scope.$on '$destroy', ->
-        $window.ytPlayer and $window.ytPlayer.destroy()
-        $window.YT = undefined
-        return
-
+    $window.onYouTubeIframeAPIReady = ->
+      ytPlayer = new $window.YT.Player 'ngYoutube',
+        videoId: playerVideoId
+        height: playerHeight
+        width: playerWidth
+        playerVars: playerVars
+        events:
+          onReady: onPlayerReady
+          onPlaybackQualityChange: onPlayerPlaybackQualityChange
+          onStateChange: onPlayerStateChange
+          onError: onPlayerError
+          onPlaybackRateChange: onPlaybackRateChange
+          onApiChange: onApiChange
       return
-]
+    ###
+    # events emit by youtube iframe api
+    ###
+
+    onPlayerReady = (event) ->
+      scope.$emit 'ngYoutubePlayer:onPlayerReady', event
+      return
+
+    onPlayerPlaybackQualityChange = (event) ->
+      scope.$emit 'ngYoutubePlayer:onPlayerPlaybackQualityChange', event
+      return
+
+    onPlayerStateChange = (event) ->
+      scope.$emit 'ngYoutubePlayer:onPlayerStateChange', event
+
+      switch event.data
+        when $window.YT.PlayerState.PLAYING
+          scope.$emit 'ngYoutubePlayer:PLAYING', event
+        when $window.YT.PlayerState.ENDED
+          scope.$emit 'ngYoutubePlayer:ENDED', event
+        when $window.YT.PlayerState.UNSTARTED
+          scope.$emit 'ngYoutubePlayer:UNSTARTED', event
+        when $window.YT.PlayerState.PAUSED
+          scope.$emit 'ngYoutubePlayer:PAUSED', event
+        when $window.YT.PlayerState.BUFFERING
+          scope.$emit 'ngYoutubePlayer:BUFFERING', event
+        when $window.YT.PlayerState.CUED
+          scope.$emit 'ngYoutubePlayer:CUED', event
+      return
+
+    onPlayerError = (event) ->
+      scope.$emit 'ngYoutubePlayer:onPlayerError', event
+      return
+
+    onPlaybackRateChange = (event) ->
+      scope.$emit 'ngYoutubePlayer:onPlaybackRateChange', event
+      return
+
+    onApiChange = ->
+      scope.$emit 'ngYoutubePlayer:onApiChange', ytPlayer
+      return
+
+    scope.$on '$destroy', ->
+      $window.ytPlayer and $window.ytPlayer.destroy()
+      $window.YT = undefined
+      return
+
+    return
+
+###
+# dependency injection
+###
+$youtube.$inject = ['$compile', 'ytPlayer', 'youtubePlayerConfig', '$window']
+
+###
+# define angular application
+###
+angular.module 'ngYoutube', ['ngSanitize']
+  .directive('youtube', $youtube)
+  .constant('youtubePlayerConfig', $youtubePlayerConfig)
+  .constant 'ytPlayer'

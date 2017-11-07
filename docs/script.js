e2f9e075cb9535f9279e815232a106a0c9856f5a
Sibiraj <sibiraj_s@live.com>
2017-11-08 02:05:59 +0530

    docs: use strict-mode

--- docs/script.js
+++ docs/script.js
@@ -3,20 +3,20 @@
 var app = angular.module('myApp', ['ngYoutube']);
 
 app.controller('mainCtrl', ['$scope', 'ytPlayer',
-function ($scope, ytPlayer) {
+    function ($scope, ytPlayer) {
 
-    $scope.playerReady = false;
-    $scope.ytPlayer;
+        $scope.playerReady = false;
+        $scope.ytPlayer;
 
-    $scope.videoId = 'iGk5fR-t5AU';
+        $scope.videoId = 'iGk5fR-t5AU';
 
-    $scope.$on('ngYoutubePlayer:onPlayerReady', function(event, data, id) {
-        $scope.ytPlayer = ytPlayer;
-        console.log($scope.ytPlayer['player1'])
-        if($scope.ytPlayer['player1']){
-            $scope.playerReady = true;
-            $scope.$apply();
-        }
-    });
+        $scope.$on('ngYoutubePlayer:onPlayerReady', function (event, data, id) {
+            $scope.ytPlayer = ytPlayer;
 
-}]);
+            if ($scope.ytPlayer['player1']) {
+                $scope.playerReady = true;
+                $scope.$apply();
+            }
+        });
+
+    }]);

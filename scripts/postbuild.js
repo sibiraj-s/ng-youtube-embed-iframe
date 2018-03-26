0ffef3221485d695680ac5f1df485d08192b31b1
Sibiraj <dev.sibiraj@outlook.com>
2018-03-26 16:01:15 +0530

    build: add postbuild actions

---
+++ scripts/postbuild.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+fs.readFile('dist/package.json', 'utf8', function (err, data) {
+
+    if (err) throw err;
+
+    const pkg = JSON.parse(data);
+    pkg['main'] = 'ng-youtube.min.js';
+    delete pkg['scripts'];
+
+    fs.writeFile('dist/package.json', JSON.stringify((pkg), null, 2), 'utf8', function (err) {
+        if (err) throw err;
+    });
+
+});
